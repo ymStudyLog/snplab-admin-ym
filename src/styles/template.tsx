77052954add26c31d3e4d72f0accae507ba90b8f
ymStudyLog <ylee585@gmail.com>
2022-07-22 11:04:21 +0900

    feat : template 수정

--- src/styles/template.tsx
+++ src/styles/template.tsx
@@ -4,11 +4,25 @@ type Props = {
   children: React.ReactNode
 };
 
-const HorizontalCenterDiv = styled.div`
-  width: 100%;
+const CenteredDiv = styled.div`
+  width: 350px;
+  height: 100vh;
+  overflow-y: scroll;
   display: flex;
   justify-content: center;
   background-color: white;
+  align-items: center;
+  position: fixed;
 `;
 
-export const PageTemplate = (props : Props) => <HorizontalCenterDiv {...props}>{props.children}</HorizontalCenterDiv>
+export const PageTemplate = (props : Props) => <CenteredDiv {...props}>{props.children}</CenteredDiv>
+
+//350px 변경 예정
+const NarrowContainer = styled.div`
+  max-width: 480px;
+  text-align: center;
+  border-left: 1px solid #d4d4d4;
+  border-right: 1px solid #d4d4d4;
+`;
+
+export const MobileSizeContainer = (props: Props) => <NarrowContainer {...props}>{props.children}</NarrowContainer>
