3314f49b08f0205fe7d8c621bce2ec5752b6fed2
sgsg9447 <sgsg9447@gmail.com>
2022-07-22 22:55:49 +0900

    feat #5 : form-validcheck 완료

--- src/utils/Regex.tsx
+++ src/utils/Regex.tsx
@@ -4,10 +4,13 @@ const nickname = /^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]{3,10}$/;
 //email형식
 const email = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
 
-//영어소문자+숫자+특수문자(_,-)+글자수(6글자 이상, 10글자 이하)
+//phone형식 (숫자만, 11글자 이하)
 const phone =  /^[0-9]{11}/g;
 
-const regex = { nickname, email, phone };
+const date = /^(([0-9]+).{4}([0-9]+).{2}([0-9]+){2})/g;
+
+
+const regex = { nickname, email, phone, date };
 
 export default regex;
 
